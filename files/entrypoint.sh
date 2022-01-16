7b31f11162ee5c4b9219bfced144a5f58bafb5f3
root <root@vultr.guest>
2022-01-16 14:58:14 +0000

    make it work

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -1,4 +1,4 @@
-#/usr/bin/bash
+#!/usr/bin/bash
 # Run commands in the Docker container with a particular UID and GID.
 # The idea is to run the container like
 #   docker run -i \
@@ -7,30 +7,32 @@
 #     image-name /bin/zsh
 # where the -e flags pass the env vars to the container, which are read by this script.
 # By setting copying this script to the container and setting it to the
+
 # ENTRYPOINT, and subsequent commands run in the container will run as the user
 # who ran `docker` on the host, and so any output files will have the correct
 # permissions.
 
-# USER and GROUP to run 
+# USER and GROUP to run
 USER=${USER:-devuser}
 GROUP=${GROUP:-$USER}
+PASSWD=${PASSWD:-passwd01}
 
 export HOME=/home/$USER
 
 # add new user if $USER not exists
 if [ "$USER" != "root" ]; then
     echo User $USER is not found, create it
-    groupadd -g $GROUP groupid
-    adduser --quiet --disabled-password --shell /bin/zsh --home /home/$USER --gecos "User" --gid $GROUP $USER 
-    echo "$USER:$PASSWD" | chpasswd
-    usermod -aG sudo $USER
+    addgroup --gid 1000 $GROUP \
+    && adduser --quiet --disabled-password --shell /bin/zsh --home /home/$USER --gecos "User" --uid 1000 --gid 1000 $USER \
+    && echo "$USER:$PASSWD" | chpasswd \
+    && usermod -aG sudo $USER
 
     # copy user configuration files
-    cp -r /root/{.local,.zshrc,.profile,.oh-my-zsh,.cargo,.rustup,.config,.p10k.zsh,.zshenv,powerlevel10k} /home/$USER
+    cp -r /root/.local /root/.zshrc /root/.profile /root/.oh-my-zsh /root/.cargo /root/.rustup /root/.config /root/.p10k.zsh /root/.zshenv /root/powerlevel10k /home/$USER
     # add vim-plug
     mkdir -p /home/${USER}/.local/share/nvim/site/autoload
     cp /root/.local/share/nvim/site/autoload/plug.vim /home/${USER}/.local/share/nvim/site/autoload/plug.vim
     chown -R $USER:$USER $HOME
 fi
 
-exec /usr/local/bin/gosu $USER:$GROUP "$@"
+exec /usr/sbin/gosu $USER:$GROUP "$@"
